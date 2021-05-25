b7a415ae356c0714308c6e576cb96bedbc1069c4
Abdelrazaq Kassem <>
2021-05-25 00:01:54 +0000

    Component 1: trial 1: Created starting window

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+
+
+class QuizStarter:
+    def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
+ 
+        background_color="orange"# to set it as background color for all the label widgets
+
+        #frame set up
+        self.quiz_frame=Frame(parent, bg = background_color)
+        #padx, pady How many pixels to pad widget, horizontally (x) and vertically (y), outside widget's borders.
+        self.quiz_frame.grid()#This geometry manager organizes widgets in a table-like structure in the parent widget.
+               
+        #widgets goes below
+        self.heading_label=Label(self.quiz_frame, text="Lets test your car knowledge", font=("Tw Cen MT","18","bold"),bg=background_color)
+        self.heading_label.grid(row=0, padx=20) 
+
+        #label for username
+        self.user_label=Label(self.quiz_frame, text="Please enter your username below: ", font=("Tw Cen MT","16"),bg=background_color)
+        self.user_label.grid(row=1, padx=20, pady=20) 
+        
+        #entry box
+        self.entry_box=Entry(self.quiz_frame)
+        self.entry_box.grid(row=2,padx=20, pady=20)
+        
+        #continue button
+        self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="orange", command=self.name_collection)
+        self.continue_button.grid(row=3,  padx=20, pady=20)        
+
+       
+
+    def name_collection(self):
+        name=self.entry_box.get()
+        names.append(name) #add name to names list declared at the beginning
+        self.quiz_frame.destroy()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Lets test your car knowledge") 
+    quiz_instance = QuizStarter(root) #instantiation, making an instance of the class Quiz
+    root.mainloop()#so the frame doesnt dissapear
+ 
+    
+
